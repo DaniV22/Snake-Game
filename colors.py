77205bf276d73e191f3bc7c9eba268e3a6c22b46
DaniV22 <105554997+DaniV22@users.noreply.github.com>
2022-05-14 23:47:00 +0200

    Add files via upload

---
+++ colors.py
@@ -0,0 +1,9 @@
+'''
+A dictionary containing several RGB colors decimal code
+(RED, GREEN, BLUE)  -----> From 0 to 255
+'''
+
+COLORS = {'BLACK': (0,0,0), 'WHITE': (255, 255, 255), 'RED': (255, 0, 0),
+ 'GREEN': (0, 255, 0), 'BLUE': (0,0,255), 'DARK_GREEN': (0, 150, 0),
+ 'YELLOW': (255, 255, 20), 'DARK_GREY': (65, 65, 65), 'LIGHT_GREY': (200, 200, 200), 
+ 'LIGHT_BLUE': (170, 220, 230)}
